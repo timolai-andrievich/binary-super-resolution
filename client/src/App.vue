ed6707e4865f67d8c475a655cf87dd52b3b1323b
timolai-andrievich <timolai.andrievitch@ya.ru>
2023-11-17 00:03:24 +0300

    Added side-by-side comparison

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,11 @@ import { onMounted, ref } from "vue"
 const fileInput = ref(null)
 const sourceImg = ref(null)
 const upscaledImg = ref(null)
+const hoverTextPos = ref([0, 0])
+const sourceCanvas = ref(null)
+const upscaledCanvas = ref(null)
 let loading = ref(false)
+let showHover = ref(false)
 
 function submit() {
   let file = fileInput.value.files[0];
@@ -28,6 +32,70 @@ async function processImage(image) {
   upscaledImg.value.src = json.image;
   loading.value = false
 }
+
+function onHoverOriginal(){
+  showHover.value = true
+}
+
+function onLeaveOriginal() {
+  showHover.value = false
+}
+
+function onHoverUpscaled(){
+  showHover.value = true
+}
+
+function onLeaveUpscaled() {
+  showHover.value = false
+}
+
+function updateHoverPosition(event: any) {
+  if (sourceCanvas.value === null || upscaledCanvas.value === null) {
+    return
+  }
+  let scale = 5
+  let rect = sourceImg.value.getBoundingClientRect()
+  if (rect.top > event.clientY || rect.bottom < event.clientY || rect.left > event.clientX || rect.right < event.clientX) {
+    rect = upscaledImg.value.getBoundingClientRect()
+  }
+  let rectAspect = rect.width / rect.height
+  let imgAspect = sourceImg.value.naturalWidth / sourceImg.value.naturalHeight
+  let xPadding = 0
+  let yPadding = 0
+  if (imgAspect > rectAspect) {
+    let height = rect.width / imgAspect
+    yPadding = (rect.height - height) / 2
+  } else if (imgAspect < rectAspect) {
+    let width = rect.height * imgAspect
+    xPadding = (rect.width - width) / 2
+  }
+  let x = (event.clientX - rect.left - xPadding) / (rect.width - xPadding * 2)
+  let y = (event.clientY - rect.top - yPadding) / (rect.height - yPadding * 2)
+
+  {
+    let ctx = sourceCanvas.value.getContext('2d')
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.scale(scale, scale)
+    let dx = -(x * sourceImg.value.naturalWidth)
+    let dy = -(y * sourceImg.value.naturalHeight)
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, sourceCanvas.value.width, sourceCanvas.value.height)
+    ctx.drawImage(sourceImg.value, dx, dy)
+  }
+  {
+    let ctx = upscaledCanvas.value.getContext('2d')
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.scale(scale, scale)
+    let dx = -(x * upscaledImg.value.naturalWidth)
+    let dy = -(y * upscaledImg.value.naturalHeight)
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, upscaledCanvas.value.width, upscaledCanvas.value.height)
+    ctx.drawImage(upscaledImg.value, dx, dy)
+  }
+  hoverTextPos.value[1] = event.pageY
+  hoverTextPos.value[0] = event.pageX
+}
+
 </script>
 
 <template>
@@ -37,7 +105,9 @@ async function processImage(image) {
       <button @click="submit">Submit</button>
     </div>
     <div class="images">
-      <img ref="sourceImg">
+      <div class="original-container">
+        <img ref="sourceImg" @mousemove="updateHoverPosition" @mouseover="onHoverOriginal" @mouseleave="onLeaveOriginal">
+      </div>
       <div class="upscaled-container">
         <div :class="['upscaled-overlay', { hidden: !loading }]">
           <p>Upscaling</p>
@@ -46,16 +116,41 @@ async function processImage(image) {
             <div></div>
           </div>
         </div>
-        <img ref="upscaledImg">
+        <img ref="upscaledImg" @mousemove="updateHoverPosition" @mouseover="onHoverUpscaled" @mouseleave="onLeaveUpscaled">
       </div>
     </div>
+  </div>
+  <div class="hover-text" :style="{display: showHover ? 'inline' : 'none', top: `${hoverTextPos[1]}px`, left: `${hoverTextPos[0]}px`}" @mousemove="updateHoverPosition">
+    <canvas ref="sourceCanvas" width="100" height="100"></canvas>
+    <canvas ref="upscaledCanvas" width="100" height="100"></canvas>
   </div>
 </template>
 
 <style>
+.hover-text {
+  position: absolute;
+  transform: translate(-100%, -100%);
+  display: flex;
+  flex-direction: row;
+  column-gap: 1rem;
+}
+
+.hover-text > * {
+  flex: 1;
+}
+
+html, body {
+  height: 100%;
+}
+
 .container {
   display: flex;
   flex-direction: column;
+  height: 100%;
+}
+
+.inputs {
+  padding: 1rem;
 }
 
 #app {
@@ -64,14 +159,26 @@ async function processImage(image) {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 
 .images {
   display: flex;
   flex-direction: row;
   width: 100%;
-  justify-content: center;
+  height: 100%;
+  justify-content: stretch;
+}
+
+.images > * {
+  flex: 1;
+  display: flex;
+}
+
+img {
+  flex: 1;
+  object-fit:contain;
+  max-width: 100%;
 }
 
 .upscaled-container {
