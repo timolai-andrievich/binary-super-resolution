8dba45113a98d0f78e94504e0196f4e0e8cbe94f
timolai-andrievich <timolai.andrievitch@ya.ru>
2023-11-29 10:37:34 +0300

    Squashed commit of the following:
    commit 51b2c96343bf534a8fdd651e2431299ea9de1d17
    Merge: 8c1fa4f 69629b6
    Author: timolai-andrievich <timolai.andrievitch@ya.ru>
    Date:   Wed Nov 29 10:36:59 2023 +0300
    
        Merge remote-tracking branch 'origin/bbcu' into dev
    
    commit 8c1fa4f062a20cdb734b149d8c8957fb67943cbd
    Author: timolai-andrievich <timolai.andrievitch@ya.ru>
    Date:   Wed Nov 29 10:36:13 2023 +0300
    
        Some minor improvements to UI
    
    commit 69629b648177068b757ec10e5f2e9ac397e42847
    Author: Evgenij Ivankin <e.ivankin@innopolis.university>
    Date:   Mon Nov 27 23:33:44 2023 +0300
    
        experiments with bbcu
    
    commit ed6707e4865f67d8c475a655cf87dd52b3b1323b
    Author: timolai-andrievich <timolai.andrievitch@ya.ru>
    Date:   Fri Nov 17 00:03:24 2023 +0300
    
        Added side-by-side comparison
    
    commit a6559f68ace14939c6835832e66053a1a15327ed
    Author: timolai-andrievich <timolai.andrievitch@ya.ru>
    Date:   Thu Nov 16 16:32:27 2023 +0300
    
        Removed eslint and changed the name of the app
    
    commit e2f7f68caef77809c85bc1a098a0bf40d49815dc
    Author: timolai-andrievich <timolai.andrievitch@ya.ru>
    Date:   Fri Nov 10 13:05:16 2023 +0300
    
        Added loader animation and side-by-side layout

--- app.py
+++ app.py
@@ -30,9 +30,21 @@ def decode_image(data_url: str) -> Image.Image:
     data_bytes = base64.b64decode(data)
     bytesIO = io.BytesIO(data_bytes)
     img = Image.open(bytesIO)
+    img = remove_alpha(img)
     return img
 
-model = load.load('scale2x.ckpt', scale=2)
+def remove_alpha(img: Image.Image) -> Image.Image:
+    if img.mode in ('RGBA', 'LA') or (img.mode == 'P' and 'transparency' in img.info):
+        alpha = img.convert('RGBA').split()[-1]
+        bg = Image.new("RGBA", img.size, (255, 255, 255, 255))
+        bg.paste(img, mask=alpha)
+        return bg.convert('RGB')
+    else:
+        return img
+    
+    
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = load.load('scale2x.ckpt', scale=2).to(device)
 
 @app.route('/')
 def index():
@@ -53,7 +65,10 @@ def upscale():
     img = decode_image(url)
     image_tensor = torch.tensor(np.array(img).transpose((2, 0, 1)))
     image_tensor = torch.unsqueeze(image_tensor, 0)
-    upscaled = model(image_tensor).detach().numpy()[0].transpose((1, 2, 0))
+    image_tensor = image_tensor.to(device)
+    with torch.no_grad():
+        model.eval()
+        upscaled = model(image_tensor).detach().cpu().numpy()[0].transpose((1, 2, 0))
     upscaled = np.clip(upscaled, 0, 255).astype('uint8')
     img = Image.fromarray(upscaled)
     url = encode_image(img)
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,111 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-const fileInput = ref(null);
-const img = ref("");
-let loading = false;
+// TODO move hovered lens when approaching the edge of the screen
+// TODO multiple scales
+// TODO download button
+// TODO cancel upscaling button, and cancelling on server error
+// TODO error message
+import { ref, computed } from "vue"
+const fileInput = ref(null)
+const sourceImg = ref(null)
+const upscaledImg = ref(null)
+const hoverTextPos = ref([0, 0])
+const sourceCanvas = ref(null)
+const upscaledCanvas = ref(null)
+const loading = ref(false)
+const mouseOnCanvas = ref(false)
+const errorMessage = ref(null);
+const showHover = computed(() => errorMessage.value === null && !loading.value && mouseOnCanvas.value && upscaledImg && upscaledImg.value && upscaledImg.value.src)
 
 function submit() {
   let file = fileInput.value.files[0];
   let reader = new FileReader();
   if (file === undefined) {
-    return
+    return;
   }
   reader.readAsDataURL(file);
-  reader.onload = e => processImage(e.target.result)
+  reader.onload = (e) => processImage(e.target.result);
 }
 
 async function processImage(image) {
-  loading = true;
-  let response = await fetch('/upscale', { method: "POST", mode: "no-cors", body: JSON.stringify({ image: image }) })
-  let json = await response.json()
-  img.value = json.image
-  loading = false;
+  loading.value = true
+  upscaledImg.value.src = image
+  sourceImg.value.src = image
+  let response = await fetch("/upscale", {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify({ image: image }),
+  });
+  if (!response.ok) {
+    errorMessage.value = "Error: Unable to connect to the server. Please check your internet connection and ensure that the server is currently accessible. "
+    loading.value = false
+    return
+  }
+  let json = await response.json();
+  upscaledImg.value.src = json.image;
+  loading.value = false
+}
+
+function onHoverOriginal(){
+  mouseOnCanvas.value = true
+}
+
+function onLeaveOriginal() {
+  mouseOnCanvas.value = false
+}
+
+function onHoverUpscaled(){
+  mouseOnCanvas.value = true
+}
+
+function onLeaveUpscaled() {
+  mouseOnCanvas.value = false
+}
+
+function updateHoverPosition(event: any) {
+  if (sourceCanvas.value === null || upscaledCanvas.value === null) {
+    return
+  }
+  let scale = 5
+  let rect = sourceImg.value.getBoundingClientRect()
+  if (rect.top > event.clientY || rect.bottom < event.clientY || rect.left > event.clientX || rect.right < event.clientX) {
+    rect = upscaledImg.value.getBoundingClientRect()
+  }
+  let rectAspect = rect.width / rect.height
+  let imgAspect = sourceImg.value.naturalWidth / sourceImg.value.naturalHeight
+  let xPadding = 0
+  let yPadding = 0
+  if (imgAspect > rectAspect) {
+    let height = rect.width / imgAspect
+    yPadding = (rect.height - height) / 2
+  } else if (imgAspect < rectAspect) {
+    let width = rect.height * imgAspect
+    xPadding = (rect.width - width) / 2
+  }
+  let x = (event.clientX - rect.left - xPadding) / (rect.width - xPadding * 2)
+  let y = (event.clientY - rect.top - yPadding) / (rect.height - yPadding * 2)
+
+  {
+    let ctx = sourceCanvas.value.getContext('2d')
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.scale(scale, scale)
+    let dx = -(x * sourceImg.value.naturalWidth - sourceCanvas.value.width / scale / 2)
+    let dy = -(y * sourceImg.value.naturalHeight - sourceCanvas.value.height / scale / 2)
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, sourceCanvas.value.width, sourceCanvas.value.height)
+    ctx.drawImage(sourceImg.value, dx, dy)
+  }
+  {
+    let ctx = upscaledCanvas.value.getContext('2d')
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.scale(scale / 2, scale / 2)
+    let dx = -(x * upscaledImg.value.naturalWidth - upscaledCanvas.value.width / scale)
+    let dy = -(y * upscaledImg.value.naturalHeight - upscaledCanvas.value.width / scale)
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, upscaledCanvas.value.width, upscaledCanvas.value.height)
+    ctx.drawImage(upscaledImg.value, dx, dy)
+  }
+  hoverTextPos.value[1] = event.pageY
+  hoverTextPos.value[0] = event.pageX
 }
 
 </script>
@@ -27,19 +113,75 @@ async function processImage(image) {
 <template>
   <div class="container">
     <div class="inputs">
-      <input id="upload-file" type="file" accept=".png,.jpg" ref="fileInput" />
-      <button @click="submit">Submit</button>
+      <input id="upload-file" type="file" accept=".png,.jpg" ref="fileInput" style="display: none;"/>
+      <button @click="fileInput.click()">Upload</button>
+      <button @click="submit">Upscale</button>
     </div>
-    <div>
-      <img :src="img" />
+    <div class="images">
+      <div class="original-container">
+        <img ref="sourceImg" @mousemove="updateHoverPosition" @mouseover="onHoverOriginal" @mouseleave="onLeaveOriginal">
+      </div>
+      <div class="upscaled-container">
+        <div :class="['upscaled-overlay', { hidden: !loading }]">
+          <p>Upscaling</p>
+          <div class="la-ball-clip-rotate-multiple">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div :class="['upscaled-overlay', { hidden: errorMessage === null }]">
+          <p>{{ errorMessage }}</p>
+        </div>
+        <img ref="upscaledImg" @mousemove="updateHoverPosition" @mouseover="onHoverUpscaled" @mouseleave="onLeaveUpscaled">
+      </div>
     </div>
+  </div>
+  <div class="hover-text" :style="{display: showHover ? 'inline' : 'none', top: `${hoverTextPos[1]}px`, left: `${hoverTextPos[0]}px`}" @mousemove="updateHoverPosition">
+    <canvas ref="sourceCanvas" width="100" height="100"></canvas>
+    <canvas ref="upscaledCanvas" width="100" height="100"></canvas>
   </div>
 </template>
 
 <style>
+.inputs > button {
+  border-radius: 1rem;
+  padding: 0.40rem;
+  background-color: white;
+  border: .13rem black solid;
+  transition: 100ms;
+}
+
+.inputs > button:hover {
+  background-color: lightgray;
+}
+
+.hover-text {
+  position: absolute;
+  transform: translate(-100%, -100%);
+  display: flex;
+  flex-direction: row;
+}
+
+.hover-text > * {
+  flex: 1;
+}
+
+html, body {
+  height: 100%;
+}
+
 .container {
   display: flex;
   flex-direction: column;
+  height: 100%;
+}
+
+.inputs {
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  column-gap: 1rem;
+  justify-content: center;
 }
 
 #app {
@@ -48,6 +190,46 @@ async function processImage(image) {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+.images {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  justify-content: stretch;
+}
+
+.images > * {
+  flex: 1;
+  display: flex;
+}
+
+img {
+  flex: 1;
+  object-fit:contain;
+  max-width: 100%;
+}
+
+.upscaled-container {
+  position: relative;
+}
+
+.upscaled-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: rgba(255, 255, 255, .8);
+}
+
+.upscaled-overlay.hidden {
+  display: none;
 }
 </style>
